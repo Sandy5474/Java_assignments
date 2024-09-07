5da06672c59cc9b3182da8ed303547846480894d
sandy <sandeshghorpade2004@gmail.com>
2024-09-07 06:07:27 +0530

    day9

--- day9/program3.java
+++ day9/program3.java
@@ -2,14 +2,15 @@ class Demo {
    public static void main(String[] args) {
   char ch1='A';
 char ch2='a';
-    for (int i = 0; i < 4; i++) {
-        for (int j = i+1; j <=4; j++) {
+    for(int i=4;i>0;i--){
+		for(int j=0;j<i;j++){
+
             if(i%2==0){
-		System.out.print(ch1);
-		ch1++;
+		System.out.print((char)(ch1+j));
+		
 }else{
-		System.out.print(ch2);
-		ch2++;
+		System.out.print((char)(ch2+j));
+		
 }
            
         }
---
+++ day9/program4.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[] args){
+	char ch='A';
+		for(int i=4;i>0;i--){
+		for(int j=0;j<i;j++){
+			System.out.print((char)(ch+j));
+}
+System.out.println();
+}
+
+}
+}
